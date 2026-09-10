7ba5ac5846daabc9dc79357812098d3e516388af
Tuukka Rintala <tuukka.rintala@tuni.fi>
2026-09-10 12:26:35 +0300

    Doubly-linked list.

---
+++ Code/Collections/DoublyLinkedList.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GA.Collections
+{
+	public class DoublyLinkedList<T> : ICollection<T>
+	{
+		protected class Node
+		{
+			public T Value { get; set; }
+			public Node Next { get; set; }
+			public Node Previous { get; set; }
+
+			public Node() : this(default(T))
+			{
+			}
+
+			public Node(T value, Node next = null, Node previous = null)
+			{
+				Value = value;
+				Next = next;
+				Previous = previous;
+			}
+		}
+
+		/// <summary>
+		/// The head of the linked list. When the list is empty, this will be null.
+		/// </summary>
+		protected Node Head { get; set; } = null;
+
+		/// <summary>
+		/// The tail of the linked list. When the list is empty, this will be null.
+		/// </summary>
+		protected Node Tail { get; set; } = null;
+
+		public int Count { get; private set; } = 0;
+
+		public virtual bool IsReadOnly => false;
+
+		public void Add(T item)
+		{
+			if (IsReadOnly)
+			{
+				throw new System.NotSupportedException("The collection is read-only.");
+			}
+
+			Node node = new Node(item);
+
+			if (Head == null)
+			{
+				Head = node;
+				Tail = node;
+			}
+			else
+			{
+				node.Previous = Tail;
+				Tail.Next = node;
+				Tail = node;
+			}
+
+			Count++;
+		}
+
+		public void Clear()
+		{
+			if (IsReadOnly)
+			{
+				throw new System.NotSupportedException("The collection is read-only.");
+			}
+
+			Head = null;
+			Tail = null;
+			Count = 0;
+		}
+
+		public bool Contains(T item)
+		{
+			Node current = Head;
+			
+			while (current != null)
+			{
+				if (EqualityComparer<T>.Default.Equals(current.Value, item))
+				{
+					return true;
+				}
+
+				current = current.Next;
+			}
+
+			return false;
+		}
+
+		public virtual void CopyTo(T[] array, int arrayIndex)
+		{
+			throw new System.NotImplementedException("Not necessary for this example :D");
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			Node current = Head;
+			while (current != null)
+			{
+				yield return current.Value;
+				current = current.Next;
+			}
+		}
+
+		public bool Remove(T item)
+		{
+			if (IsReadOnly)
+			{
+				throw new System.NotSupportedException("This collection is read-only");
+			}
+
+			// Added next Node for linking that Node's previous while removing.
+
+			Node current = Head;
+			Node previous = null;
+			Node next = Head.Next;
+
+			while (current != null)
+			{
+				if (EqualityComparer<T>.Default.Equals(current.Value, item))
+				{
+					if (previous != null)
+					{
+						// Removing any other element than the first.
+						previous.Next = current.Next;
+						next.Previous = current.Previous;
+					}
+					else
+					{
+						// Removing the first element.
+						Head = current.Next;
+						next.Previous = Head;
+					}
+
+					Count--;
+					return true;
+				}
+				
+				// Move to the next node in the list, and note its neighours
+				previous = current;
+				current = current.Next;
+				next = current.Next;
+			}
+
+			return false;
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+	}
+}
